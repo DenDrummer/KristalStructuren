b9d23739c15624bcac5d1cdaf8efc53e617ee351
MarnickH <marnick.huysmans@student.kdg.be>
2018-12-06 15:16:44 +0100

    basic structure and teleport

---
+++ Assets/MyAssets/Scripts/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Teleport : MonoBehaviour,IPointerClickHandler {
+
+    private TeleportPlayer teleportPlayer;
+
+    // Use this for initialization
+    void Start () {
+        teleportPlayer = GameObject.Find("Player").GetComponent<TeleportPlayer>();
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            teleportPlayer.Teleport(transform.position,transform.lossyScale.x);
+        }
+    }
+}
---
+++ Assets/MyAssets/Scripts/TeleportPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour {
+
+    List<GameObject> gameObjects = new List<GameObject>();
+
+    public void Teleport(Vector3 position, float size)
+    {
+        gameObjects.ForEach(g => g.SetActive(true));
+        gameObjects.Clear();
+
+        transform.position = position;
+        Debug.Log(position);
+
+        Collider[] colliders = Physics.OverlapSphere(position, size / 2 + 0.1f);
+        new List<Collider>(colliders).ForEach(c => gameObjects.Add(c.gameObject));
+        gameObjects.ForEach(g => g.SetActive(false));
+    }
+}
