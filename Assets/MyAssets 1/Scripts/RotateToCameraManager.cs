df8004590943e4924a42b36d5855506e28f4ea93
Sam Laureys <sam.laureys@student.kdg.be>
2018-12-07 12:16:47 +0100

    Sammy init

---
+++ Assets/MyAssets 1/Scripts/RotateToCameraManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToCameraManager : MonoBehaviour {
+
+	void Start () {
+        transform.LookAt(Camera.main.transform);
+    }
+}
