b15bbd1372db5becb2992bc003e558745942afa4
MarnickH <marnick.huysmans@student.kdg.be>
2019-01-13 00:13:44 +0100

    Cif Menu

--- Assets/MyAssets Jorden/Scripts/UserStats.cs
+++ Assets/MyAssets Jorden/Scripts/UserStats.cs
@@ -2,9 +2,11 @@
 public static class UserStats
 {
     public static State State { get; set; }
+    public static string Cif { get; set; }
 
     static UserStats()
     {
         State = State.Default;
+        Cif = ""; //default cif file goes here
     }
 }
