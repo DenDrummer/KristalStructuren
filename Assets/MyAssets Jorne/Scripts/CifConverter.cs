29aac676df356c141694c40cdd90de475b5892eb
MarnickH <marnick.huysmans@student.kdg.be>
2019-01-13 20:11:04 +0100

    working cif

--- Assets/MyAssets Jorne/Scripts/CifConverter.cs
+++ Assets/MyAssets Jorne/Scripts/CifConverter.cs
@@ -4,9 +4,9 @@ using System.IO;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
-public class CifConverter : MonoBehaviour
+//Converts .cif files to Structures 
+public class CifConverter
 {
-
     private float a;
     private float b;
     private float c;
@@ -17,41 +17,40 @@ public class CifConverter : MonoBehaviour
     private Dictionary<string, List<string>> symmetry = new Dictionary<string, List<string>>();
 
     public Transform atom;
-    private void Start()
+    public CifConverter(Transform atom) {
+        this.atom = atom;
+    }
+
+    public Structure getStructure(string pathName)
     {
-        string path = @"D:\Downloads\testje.cif";
+        
+        string path = pathName;
         Structure returnStructuur;
         Convert(path);
-        Debug.Log(a);
-        Debug.Log(b);
-        Debug.Log(c);
-        /*Debug.Log(alpha);
-        Debug.Log(beta);
-        Debug.Log(gamma);
-        Debug.Log("stuff");*/
-        
-        
         List<Atom> atomen = new List<Atom>();
-
         List<Element> elements = new List<Element>();
-        List<string> Xwaarden=null;
-        List<string> Ywaarden=null;
-        List<string> Zwaarden=null;
-        foreach (KeyValuePair<string, List<string>> entry in atoms) {
-            
-            if (entry.Key.Equals("_atom_site_fract_x")) {
-                Xwaarden = entry.Value;
+        List<string> XValues = null;
+        List<string> YValues = null;
+        List<string> ZValues = null;
+
+        //Initializing X,Y,Z and Elements
+        foreach (KeyValuePair<string, List<string>> entry in atoms)
+        {
+
+            if (entry.Key.Equals("_atom_site_fract_x"))
+            {
+                XValues = entry.Value;
             }
 
 
             if (entry.Key.Equals("_atom_site_fract_y"))
             {
-                Ywaarden = entry.Value;
+                YValues = entry.Value;
             }
 
             if (entry.Key.Equals("_atom_site_fract_z"))
             {
-                Zwaarden = entry.Value;
+                ZValues = entry.Value;
             }
 
             if (entry.Key.Equals("_atom_site_label"))
@@ -63,11 +62,16 @@ public class CifConverter : MonoBehaviour
             }
 
         }
-        for (int i = 0; i < Xwaarden.Count; i++) {
-            Atom atoom = new Atom(double.Parse(Xwaarden[i])*a, double.Parse(Ywaarden[i])*b, double.Parse(Zwaarden[i])*c,elements[i].abbreviation);
+        
+        //Setting initial Atoms
+        for (int i = 0; i < XValues.Count; i++)
+        {
+            Atom atoom = new Atom(double.Parse(XValues[i]) * a, double.Parse(YValues[i]) * b, double.Parse(ZValues[i]) * c, elements[i].abbreviation);
+            //Debug.Log(atoom.element.abbreviation);
             atomen.Add(atoom);
         }
-        //Hoekpunten van atomen kopiëren
+        //No use of symmetry so instead there is this,
+        //Copying cornerpoints of atoms, for cubic structures
         for (int i = 0; i < atomen.Count; i++) {
             if (atomen[i].x == 0) {
                 atomen.Add(new Atom(a, atomen[i].y, atomen[i].z,atomen[i].element.abbreviation));
@@ -82,143 +86,111 @@ public class CifConverter : MonoBehaviour
             }
         }
         returnStructuur = new Structure(atomen, a, atom);
-        returnStructuur.drawEveryLayer();
+       
 
-        /*foreach (KeyValuePair<string, List<string>> entry in atoms)
-        {
-            
-            if (entry.Key.Equals("_atom_site_label")) {
-                foreach (string item in entry.Value) {
-                    //Debug.Log(item.Substring(0,2));
-                    elements.Add(AllElements.getElement(item.Substring(0, 2)));
-                }
-            }
-
-            if (entry.Key.Equals("_atom_site_fract_x"))
-            {
-                foreach (string item in entry.Value)
-                {
-                    Debug.Log(item);
-                }
-            }
-
-            if (entry.Key.Equals("_atom_site_fract_y"))
-            {
-                foreach (string item in entry.Value)
-                {
-                    
-                }
-            }
-
-            if (entry.Key.Equals("_atom_site_fract_z"))
-            {
-                foreach (string item in entry.Value)
-                {
-                   
-                }
-            }
-        }*/
+        return returnStructuur;
     }
 
-    public void Convert(string path)
+    //Converts cif file to Symmetry and Atoms Dictioniaries
+    private void Convert(string path)
     {
         atoms.Clear();
         symmetry.Clear();
         RegexOptions options = RegexOptions.None;
         Regex regex = new Regex("[ ]{2,}", options);
-        
 
+        
         using (StreamReader sr = File.OpenText(path))
         {
             string line = "";
             while (sr.Peek() >= 0)
             {
-               // Debug.Log(line);
                 if (line.Contains("loop_"))
                 {
-                    line = sr.ReadLine();
-                    if (line.StartsWith("_atom_site_label"))
-                    {
-                        while (line.StartsWith("_"))
-                        {
-                            //Debug.Log(line);
-                            atoms.Add(line.Trim(), new List<string>());
-                            //Debug.Log("atom key added");
-                            line = sr.ReadLine();
-                        }
-                        while (line != null && (!line.Trim().Equals("") && !line.Contains("loop_") && !line.Contains("#End of data") && !line.StartsWith("_")))
-                        {
-                            //Debug.Log(line);
-                            string[] values = line.Split(" ".ToCharArray(), atoms.Count);
-                            int i = 0;
-                            foreach (var keyValuePair in atoms)
-                            {
-                                keyValuePair.Value.Add(values[i]);
-                                i++;
-                            }
-                            line = sr.ReadLine();
-                        }
-                    }
-                    else if (line.StartsWith("_symmetry_equiv_pos"))
-                    {
-                        while (line.StartsWith("_"))
-                        {
-                            //Debug.Log(line);
-                            symmetry.Add(line.Trim(), new List<string>());
-                            //Debug.Log("symmetry key added");
-                            line = sr.ReadLine();
-                        }
-                        while (line != null && (!line.Trim().Equals("") && !line.Contains("loop_") && !line.Contains("#End of data") && !line.StartsWith("_")))
-                        {
-                            //Debug.Log(line);
-                            string[] values = line.Split(" ".ToCharArray(), symmetry.Count);
-                            int i = 0;
-                            foreach (var keyValuePair in symmetry)
-                            {
-                                keyValuePair.Value.Add(values[i]);
-                                i++;
-                            }
-                            line = sr.ReadLine();
-                        }
-                    }
+                    line = LoopProcessor(sr, regex);
                 }
                 else
                 {
-                    if (line.StartsWith("_"))
-                    {
-                        if (line.StartsWith("_cell_length"))
-                        {
-                            if (line.StartsWith("_cell_length_a"))
-                            {
-                                a = GetValue(line, "_cell_length_a");
-                            }
-                            else if (line.StartsWith("_cell_length_b"))
-                            {
-                                b = GetValue(line, "_cell_length_b");
-                            }
-                            else if (line.StartsWith("_cell_length_c"))
-                            {
-                                c = GetValue(line, "_cell_length_c");
-                            }
-                        }
-                        else if (line.StartsWith("_cell_angle"))
-                        {
-                            if (line.StartsWith("_cell_angle_alpha"))
-                            {
-                                alpha = GetValue(line, "_cell_angle_alpha");
-                            }
-                            else if (line.StartsWith("_cell_angle_beta"))
-                            {
-                                beta = GetValue(line, "_cell_angle_beta");
-                            }
-                            else if (line.StartsWith("_cell_angle_gamma"))
-                            {
-                                gamma = GetValue(line, "_cell_angle_gamma");
-                            }
-                        }
-                    }
+                    ValueGetter(line);
                     line = sr.ReadLine();
                     line = regex.Replace(line, " ");
+                }
+            }
+        }
+    }
+
+    private string LoopProcessor(StreamReader sr, Regex regex)
+    {
+        string line = sr.ReadLine();
+        if (line.StartsWith("_atom_site_label"))
+        {
+            line = DictionaryCreator(atoms, sr, line, regex);
+        }
+        else if (line.StartsWith("_symmetry_equiv_pos"))
+        {
+            line = DictionaryCreator(symmetry, sr, line, regex);
+        }
+        return line;
+    }
+
+    private string DictionaryCreator(Dictionary<string, List<string>> dictionary, StreamReader sr, string input, Regex regex)
+    {
+        string line = input;
+        while (line.StartsWith("_"))
+        {
+            //Debug.Log(line);
+            dictionary.Add(line.Trim(), new List<string>());
+            //Debug.Log("Key added");
+            line = sr.ReadLine();
+        }
+        while (line != null && (!line.Trim().Equals("") && !line.Contains("loop_") && !line.Contains("#End of data") && !line.StartsWith("_")))
+        {
+            line = regex.Replace(line, " ");
+            //Debug.Log(line);
+            string[] values = line.Split(" ".ToCharArray(), dictionary.Count);
+            int i = 0;
+            foreach (var keyValuePair in dictionary)
+            {
+                keyValuePair.Value.Add(values[i]);
+                i++;
+            }
+            line = sr.ReadLine();
+        }
+        return line;
+    }
+
+    private void ValueGetter(string line)
+    {
+        if (line.StartsWith("_"))
+        {
+            if (line.StartsWith("_cell_length"))
+            {
+                if (line.StartsWith("_cell_length_a"))
+                {
+                    a = GetValue(line, "_cell_length_a");
+                }
+                else if (line.StartsWith("_cell_length_b"))
+                {
+                    b = GetValue(line, "_cell_length_b");
+                }
+                else if (line.StartsWith("_cell_length_c"))
+                {
+                    c = GetValue(line, "_cell_length_c");
+                }
+            }
+            else if (line.StartsWith("_cell_angle"))
+            {
+                if (line.StartsWith("_cell_angle_alpha"))
+                {
+                    alpha = GetValue(line, "_cell_angle_alpha");
+                }
+                else if (line.StartsWith("_cell_angle_beta"))
+                {
+                    beta = GetValue(line, "_cell_angle_beta");
+                }
+                else if (line.StartsWith("_cell_angle_gamma"))
+                {
+                    gamma = GetValue(line, "_cell_angle_gamma");
                 }
             }
         }
--- Assets/MyAssets Jorne/Scripts/GameController.cs
+++ Assets/MyAssets Jorne/Scripts/GameController.cs
@@ -1,31 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
     [SerializeField]
     private Transform atom;
-    List<Atom> atoms=new List<Atom>();
-    List<Atom> atoms2 = new List<Atom>();
-    Structure structure1;
+    private List<Atom> atoms=new List<Atom>();
+    private Structure structure;
     [SerializeField]
     private GameObject bondController;
+    private QuantumInstancer quantumInstancer;
+    private CifConverter cifConverter;
 
 
-    // Use this for initialization
+
     void Start() {
-        atoms.Add(new Atom(0, 0, 0,"Ni"));
-        atoms.Add(new Atom(3.0149999, 0, 0,"Ni"));
-        atoms.Add(new Atom(0, 3.0149999, 0, "Ni"));
-        atoms.Add(new Atom(0, 0, 3.0149999, "Ni"));
-        atoms.Add(new Atom(3.0149999, 3.0149999, 0, "Ni"));
-        atoms.Add(new Atom(3.0149999, 0, 3.0149999, "Ni"));
-        atoms.Add(new Atom(0, 3.0149999, 3.0149999, "Ni"));
-        atoms.Add(new Atom(3.0149999 / 2, 3.0149999 / 2, 3.0149999 / 2, "Ti"));
-        atoms.Add(new Atom(3.0149999, 3.0149999, 3.0149999, "Ni"));
-        structure1 = new Structure(atoms,(float) 3.0149999,atom);
-        structure1.drawEveryLayer();
+        cifConverter = new CifConverter(atom);
+        DrawFromCifConverter();
+        //DrawFromHardCoded();
         bondController.GetComponent<BondManager>().enabled = true;
     }
 	
@@ -33,4 +25,25 @@ public class GameController : MonoBehaviour {
 	void Update () {
 		
 	}
+    //Draws hardcoded austenite frame
+    void DrawFromHardCoded() {
+        atoms.Add(new Atom(0, 0, 0, "Ni"));
+        atoms.Add(new Atom(3.0149999, 0, 0, "Ni"));
+        atoms.Add(new Atom(0, 3.0149999, 0, "Ni"));
+        atoms.Add(new Atom(0, 0, 3.0149999, "Ni"));
+        atoms.Add(new Atom(3.0149999, 3.0149999, 0, "Ni"));
+        atoms.Add(new Atom(3.0149999, 0, 3.0149999, "Ni"));
+        atoms.Add(new Atom(0, 3.0149999, 3.0149999, "Ni"));
+        atoms.Add(new Atom(3.0149999 / 2, 3.0149999 / 2, 3.0149999 / 2, "Ti"));
+        atoms.Add(new Atom(3.0149999, 3.0149999, 3.0149999, "Ni"));
+        structure = new Structure(atoms, (float)3.0149999, atom);
+        quantumInstancer = new QuantumInstancer(structure,5);
+        quantumInstancer.drawEveryLayer();
+    }
+    //Draws from .cif file
+    void DrawFromCifConverter() {
+        structure = cifConverter.getStructure(UserStats.Cif);
+        quantumInstancer = new QuantumInstancer(structure,5);
+        quantumInstancer.drawEveryLayer();
+    }
 }
