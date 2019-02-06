bee0e05d89d9c6dfc6541543f47dc8588291d946
MarnickH <marnick.huysmans@student.kdg.be>
2019-02-06 22:01:33 +0100

    AR

---
+++ Assets/MyAssets/Scripts/ARController.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using GoogleARCore;
+
+public class ARController : MonoBehaviour {
+
+    [SerializeField]
+    private Camera FirstPersonCamera;
+
+    private GameObject Crystal { get; set; }
+
+    [SerializeField]
+    private GameObject SearchingForPlaneUI;
+
+    private List<DetectedPlane> m_AllPlanes = new List<DetectedPlane>();
+
+    private bool m_IsQuitting = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _UpdateApplicationLifecycle();
+
+        // Hide snackbar when currently tracking at least one plane.
+        Session.GetTrackables<DetectedPlane>(m_AllPlanes);
+        bool showSearchingUI = true;
+        for (int i = 0; i < m_AllPlanes.Count; i++)
+        {
+            if (m_AllPlanes[i].TrackingState == TrackingState.Tracking)
+            {
+                showSearchingUI = false;
+                break;
+            }
+        }
+
+        SearchingForPlaneUI.SetActive(showSearchingUI);
+
+        // If the player has not touched the screen, we are done with this update.
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            // Use hit pose and camera pose to check if hittest is from the
+            // back of the plane, if it is, no need to create the anchor.
+            if ((hit.Trackable is DetectedPlane) &&
+                Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+                    hit.Pose.rotation * Vector3.up) < 0)
+            {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            }
+            else
+            {
+                // Instantiate Andy model at the hit pose.
+                var crystal = Instantiate(Crystal, hit.Pose.position, hit.Pose.rotation);
+
+                // Create an anchor to allow ARCore to track the hitpoint as understanding of the physical
+                // world evolves.
+                var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                // Make Andy model a child of the anchor.
+                crystal.transform.parent = anchor.transform;
+            }
+        }
+    }
+
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Back();
+        }
+
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            const int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage("ARCore encountered a problem connecting.  Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
+                    message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+
+    public void Back()
+    {
+        UserStats.Cif = "";
+        SceneManager.LoadScene("ARStartScene");
+    }
+}
